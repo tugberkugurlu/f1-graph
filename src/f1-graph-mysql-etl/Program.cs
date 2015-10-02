4914640ec2b8a5cac278d59e97b1149de3673524
Tugberk Ugurlu <tugberk@outlook.com>
2015-10-02 09:20:18 -0700

    moved to beta7 and added mysql etl

---
+++ src/f1-graph-mysql-etl/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace F1Graph.Etl
+{
+    public class Program
+    {
+        public async Task Main(string[] args)
+        {
+        }
+    }
+}
