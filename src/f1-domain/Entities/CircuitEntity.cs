903a688ba2daacddc3a1202685ffda5757ef85b8
Tugberk Ugurlu <tugberk@outlook.com>
2015-10-04 03:04:34 -0700

    added ctor to CircuitEntity

--- src/f1-domain/Entities/CircuitEntity.cs
+++ src/f1-domain/Entities/CircuitEntity.cs
@@ -4,6 +4,29 @@ namespace F1.Domain.Entities
 {
     public class CircuitEntity : BaseEntity
     {
+        public CircuitEntity(string originId, string name, string city, string country, Location location, string url)
+        {
+            if (originId == null)
+                throw new System.ArgumentNullException("originId");
+            if (name == null)
+                throw new System.ArgumentNullException("name");
+            if (city == null)
+                throw new System.ArgumentNullException("city");
+            if (country == null)
+                throw new System.ArgumentNullException("country");
+            if (location == null)
+                throw new System.ArgumentNullException("location");
+            if (url == null)
+                throw new System.ArgumentNullException("url");
+
+            OriginId = originId;
+            Name = name;
+            City = city;
+            Country = country;
+            Location = location;
+            Url = url;
+        }
+
         public string OriginId { get; private set; }
         public string Name { get; private set; }
         public string City { get; private set; }
