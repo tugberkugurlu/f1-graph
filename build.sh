12564b1730bd77d5651b6423d562d90bc3506637
Tugberk Ugurlu <tugberk@outlook.com>
2017-04-03 22:29:26 +0100

    fix the build server config and scripts to work with v1.0.1 of .NET Core tooling

--- build.sh
+++ build.sh
@@ -1,12 +1,51 @@
 #!/bin/bash
 
-configuration=RELEASE
-scriptsDir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-artifactsDir=${scriptsDir%%/}/artifacts
-outputDir=${artifactsDir%%/}/apps
-projectDirectory=${scriptsDir%%/}/src/f1-graph-mysql-etl
-projectFilePath=${projectDirectory%%/}/project.json
+for i in "$@"
+do
+case $i in
+    -v=*|--version=*)
+    BUILDVERSION="${i#*=}"
+    shift # past argument=value
+    ;;
+    -c=*|--configuration=*)
+    CONFIGURATION="${i#*=}"
+    shift # past argument=value
+    ;;
+    --pack)
+    PACK="YES"
+    shift # past argument with no value
+    ;;
+    *)
+    ;;
+    --publish)
+    PUBLISH="YES"
+    shift # past argument with no value
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+done
 
-dnu restore $projectDirectory
-dnu build $projectFilePath --configuration $configuration --out $artifactsDir
-dnu publish $projectDirectory --configuration $configuration --out $outputDir --runtime active
+echo "BUILD VERSION = ${BUILDVERSION}"
+echo "CONFIGURATION = ${CONFIGURATION}"
+echo "PACK          = ${PACK}"
+echo "PUBLISH       = ${PUBLISH}"
+
+if [ -z ${CONFIGURATION+x} ]
+then
+    echo "No configuration is specified, defaulting to DEBUG"
+    CONFIGURATION=DEBUG
+fi
+
+if [ -z ${BUILDVERSION+x} ]
+then
+    echo "No build version is specified, defaulting to 0.0.0"
+    BUILDVERSION=0.0.0
+fi
+
+scriptsDir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+dotnet restore $scriptsDir || exit 1
+dotnet clean $scriptsDir --configuration $CONFIGURATION --verbosity normal || exit 1
+dotnet build $scriptsDir --configuration $CONFIGURATION --verbosity normal --version-suffix '' /property:VersionPrefix=$BUILDVERSION || exit 1
